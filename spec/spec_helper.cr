4a7571857cf6d26c3abefa579ff9707b824e171a
Ahmad Tolba <tolpa1@gmail.com>
2019-10-27 12:43:57 +0100

    Update spec_helper.cr

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -4,5 +4,5 @@ require "../src/resp"
 REDIS_PORT = ENV.fetch("REDIS_PORT", "6379")
 
 def info(c : Resp, section = "default")
-  # c.call("INFO", section).as String
+  c.call("INFO", section).as String
 end
