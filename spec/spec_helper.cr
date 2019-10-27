97019dca8f6e9ca41fbdd226abe7cd2bd5c04070
Ahmad Tolba <tolpa1@gmail.com>
2019-10-27 13:04:43 +0100

    Update spec_helper.cr

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -2,6 +2,7 @@ require "crotest"
 require "../src/resp"
 
 REDIS_PORT = ENV.fetch("REDIS_PORT", "6379")
+REDIS_HOST = ENV.fetch("REDIS_HOST", "0.0.0.0")
 
 def info(c : Resp, section = "default")
   c.call("INFO", section).as String
